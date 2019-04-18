6661cfa744f15785e1a583470a5f106bc2d9e264
dodevs <douglass.sousa@outlook.com.br>
2019-04-18 11:00:01 -0300

    Definido funções e adicionado pdfs de apoio

--- pso.py
+++ pso.py
@@ -2,11 +2,15 @@ import constants
 from random import uniform
 
 class Particle:
-    def __init__(self, x, y, x_velocity=0, y_velocity=0):
+    def __init__(self, x, y, x_velocity=0, y_velocity=0, fp_list=[]):
         self.x = x
         self.y = y
         self.x_velocity = x_velocity
         self.y_velocity = y_velocity
+        self.fp_list = fp_list
+
+    def calculate_pb(self):
+        self.pb = min(self.fp_list)
 
 def create_particles(qtd_particles):
     # Cria um numero determinado de novas particulas
@@ -25,8 +29,11 @@ def check_velocity_limit(velocity, domain):
     elif velocity < (domain[1] * constants.VELOCITY_LIMIT):
         velocity = domain[1] * constants.VELOCITY_LIMIT
 
-def initial_velocity(particle, domain):
+def initial_velocity(particles):
     new_velocity = 0.1 # Aqui entra a formula que eu ainda preciso compreender
+
+    for particle in particles:
+        particle.x_velocity = particle.y_velocity = new_velocity
 
     #for position in [particle.x_velocity, particle.x_velocity]:
         
@@ -37,7 +44,16 @@ def define_velocity(particle):
 
 
 def main(args):
+    from math import sin, sqrt, pow
     particles = create_particles(constants.PARTICLES_NUMBER)
+    initial_velocity(particles)
+
+    for particle in particles:
+        fp = 0.5 + (pow(sin(sqrt(pow(particle.x,2) + pow(particle.y,2)), 2) - 0.5) / pow(1 + 0.0001 * (pow(particle.x, 2) + pow(particle.y, 2)), 2))
+        particle.fp_list.append(fp)
+        particle.calculate_pb()
+
+    
 
     print(particles)
     
